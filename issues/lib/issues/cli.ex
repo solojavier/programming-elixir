1e53703a0618e7c495a650cfef072790661e0dd8
Javier Cervantes <1.27201@gmail.com>
2015-03-04 11:48:20 -0600

    Adding process method to issues module

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -6,7 +6,7 @@ defmodule Issues.CLI do
     """
 
   def run(argv) do
-    parse_args(argv)
+    argv |> parse_args |> process
   end
 
   @doc """
@@ -25,5 +25,16 @@ defmodule Issues.CLI do
       { _, [ user, project ], _ }        -> { user, project, @default_count }
       _                                  -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fecth(user, project)
   end
 end
