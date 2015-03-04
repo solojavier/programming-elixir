a760bf7e0a8e215d28efab9de48c150f92c2234f
Javier Cervantes <1.27201@gmail.com>
2015-03-04 13:43:53 -0600

    Adding basic log

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project) |> HTTPoison.get(@user_agent) |> handle_response
   end
 
@@ -10,7 +14,14 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body }}), do: { :ok, :jsx.decode(body) }
-  def handle_response({ :ok, %HTTPoison.Response{ status_code: ___, body: body }}), do: { :error, :jsx.decode(body) }
-  def handle_response({:error, %HTTPoison.Error{reason: reason}}), do: { :error, reason }
+  def handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body }}) do
+    Logger.info "Succesful response"
+    { :ok, :jsx.decode(body) }
+  end
+
+  def handle_response({ :ok, %HTTPoison.Response{ status_code: status, body: body }}) do
+    Logger.error "Error #{status} returned"
+    { :error, :jsx.decode(body) }
+  end
+
 end
